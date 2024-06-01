cacd1c0dc0e469ab38edb015ce060b51a3446e1c
vacastro <56000486+vacastro@users.noreply.github.com>
2024-06-01 17:55:42 -0300

    creacion de conexion y base de datos

---
+++ Datos/BaseDeDatosClinicaSePriseGrupo4.sql
@@ -0,0 +1,26 @@
+﻿drop database if exists ClinicaSePriseGrupo4;
+create database ClinicaSePriseGrupo4;
+use ClinicaSePriseGrupo4;
+
+create table roles(
+RolUsu int,
+NombreRol varchar(30),
+constraint primary key(RolUsu)
+);
+
+insert into roles values
+(001,'Administrador'),
+(002,'Empleado');
+
+create table usuario(
+CodUsu int auto_increment,
+NombreUsu varchar (20),
+PassUsu varchar (15),
+RolUsu int,
+Activo boolean default true,
+constraint pk_usuario primary key (CodUsu),
+constraint fk_usuario foreign key(RolUsu) references roles(RolUsu)
+);
+
+insert into usuario(CodUsu,NombreUsu,PassUsu,RolUsu) values
+(1,'adm','1234',001);
---
+++ Datos/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualBasic;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grupo4_ClinicaSePrise.Datos
+{
+    internal class Conexion
+    {
+        private string baseDatos;
+        private string servidor;
+        private string puerto;
+        private string usuario;
+        private string clave;
+        private static Conexion? con = null;
+
+        private Conexion() // asignamos valores a las variables de la conexion
+        {
+            this.baseDatos = "ClubDeportivoGrupo12";
+            this.servidor = "localhost";
+            this.puerto = "3306";
+            this.usuario = "root";
+            this.clave = "";
+        }
+
+        // proceso de interacción
+        public MySqlConnection CrearConcexion()
+        {
+            // instanciamos una conexion
+            MySqlConnection? cadena = new MySqlConnection();
+            // el bloque try permite controlar errores
+            try
+            {
+                cadena.ConnectionString = "datasource=" + this.servidor +
+                ";port=" + this.puerto +
+                ";username=" + this.usuario +
+                ";password=" + this.clave +
+                ";Database=" + this.baseDatos;
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw;
+            }
+            return cadena;
+        }
+        // para evaluar la instancia de la conectividad
+        public static Conexion getInstancia()
+        {
+            if (con == null) // quiere decir que la conexion esta cerrada
+            {
+                con = new Conexion(); // se crea una nueva
+            }
+            return con;
+        }
+
+    }
+}
